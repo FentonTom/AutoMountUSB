27e64cce30cbb1c1f4a9730f450f59f3d0feec6a
FentonTom <tom.fenton@gmail.com>
2021-01-06 11:33:45 -0800

    Update AutoMountUSB.bash
    Quote marks missing in initial load.

--- AutoMountUSB.bash
+++ AutoMountUSB.bash
@@ -1,8 +1,9 @@
 #/bin/bash
-## AutoMountUSB.bash
 ## set -x
+## AutoMountUSB.bash
 ## USE THIS WITH EXTREME CAUTION AND NOT ON PRODUCTION SYSTEMS!!
 ## READ THE ENTIRE SCRIPT AND UNDERSTAND WHAT IT WILL BE DOING TO YOUR SYTEM BEFORE USING IT
+## Tom Fenton V1.0 1/6/2021
 ##
 ## This script will add USB storage to an ESXi 7.0 host.
 ## For this script to work you will need add the USB storage during this script
@@ -20,12 +21,12 @@ rm /tmp/MyUSB02 2>/dev/null
 chkconfig usbarbitrator off
 ##
 ## get list of USB devices before new storage is added
-echo -e \n list of USB devices currently on system
+echo -e "\n list of USB devices currently on system "
 lsusb | sort
 lsusb | sort | grep Bus | sort > /tmp/MyUSB01
 ##
 ## get list of disks before new storage is added
-echo -e \n list of disks devices currently on system
+echo -e "\n list of disks devices currently on system "
 ls /dev/disks | grep mpx | sort
 ls /dev/disks | grep mpx | sort > /tmp/MyDisks01
 read -p  "Insert the new storage device and Press [Enter] key to continue "
@@ -66,29 +67,28 @@ NewDisk=`diff /tmp/MyDisks01 /tmp/MyDisks02 | grep ^+mpx | grep 0$ | sed s/^.//`
 ## TJF uncomment this next line to test script
 ## NewDisk=mpx.vmhba33:C0:T0:L0
 echo  The new disk is $NewDisk
-echo  The new disk is $NewDisk
 partedUtil mklabel /dev/disks/$NewDisk gpt
 ## Calculate end sector
-DiskSize=`partedUtil getptbl /dev/disks/$NewDisk | head -2 | tail -1 | cut -f 1 -d  `
+DiskSize=`partedUtil getptbl /dev/disks/$NewDisk | head -2 | tail -1 | cut -f 1 -d  " "`
 NewDS=$(($DiskSize * 255 * 63 -1))
 echo  End of new disk it $NewDS
 echo  Below is a list of the volumes currently on your system
-esxcli storage vmfs extent list | cut -f 1 -d
-echo -n -e \n Enter name of new volume here. Do not use spaces in volume name. ==>
+esxcli storage vmfs extent list | cut -f 1 -d " "
+echo -n -e "\n Enter name of new volume here. Do not use spaces in volume name."
 read VName
-echo -e \n Name of new volume will be $VName
+echo -e "\n Name of new volume will be $VName"
 ##
-partedUtil setptbl /dev/disks/$NewDisk gpt 1 2048 $NewDS AA31E02A400F11DB9590000C2911D1B8 0
+partedUtil setptbl /dev/disks/$NewDisk gpt "1 2048 $NewDS AA31E02A400F11DB9590000C2911D1B8 0"
 ## format the partition with a VMFS 6 filesystem on the device by entering
 vmkfstools -C vmfs6 -S $VName /dev/disks/$NewDisk:1
 ## Display more information about the filesystems on the ESXi host
-echo -e \n list filesystems
+echo -e "\n list filesystems"
 esxcli storage filesystem list
-echo -e \n list vmfs
+echo -e "\n list vmfs"
 esxcli storage vmfs extent list
 ## Clean up any old files
 rm /tmp/MyDisks01
 rm /tmp/MyDisks02
 rm /tmp/MyUSB01
 rm /tmp/MyUSB02
-echo -e \n Storage Script Finished \n
+echo -e "\n Storage Script Finished \n"
